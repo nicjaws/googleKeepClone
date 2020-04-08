20ffb8ff64b5d62013a395cb541979540c6f0c72
Nicolas Corbalan <outreef@hotmail.com>
2020-04-08 19:11:32 +0200

    creating notes and mapping

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 class App {
   constructor() {
-    this.notes = []
+    this.notes = [];
 
-
-    this.$form = document.querySelector('#form');
-    this.$noteTitle = document.querySelector('#note-title');
-    this.$noteTitle = document.querySelector('#note-text');
-    this.$formButtons = document.querySelector('#form-buttons');
-
+    this.$placeholder = document.querySelector('#placeholder');
+    this.$form = document.querySelector("#form");
+    this.$notes = document.querySelector('#notes');
+    this.$noteTitle = document.querySelector("#note-title");
+    this.$noteText = document.querySelector("#note-text");
+    this.$formButtons = document.querySelector("#form-buttons");
 
     this.addEventListeners();
-
   }
 
   addEventListeners() {
-    document.body.addEventListener('click', event => {
+    document.body.addEventListener("click", event => {
       this.handleFormClick(event);
     });
 
-    this.$form.addEventListener('submit', event => {
+    this.$form.addEventListener("submit", event => {
       event.preventDefault();
       const title = this.$noteTitle.value;
       const text = this.$noteText.value;
-      const hasNote = title || text; 
+      const hasNote = title || text;
       if (hasNote) {
-        //add note
-        this.addNote({title, text})
+        // add note
+        this.addNote({ title, text });
       }
-    })
+    });
   }
 
   handleFormClick(event) {
@@ -40,21 +39,46 @@ class App {
     }
   }
 
-    openForm() {
-      this.$form.classList.add('form-open');
-      this.$noteTitle.style.display = 'block';
-      this.$formButtons.style.display = 'block';
-    }
+  openForm() {
+    this.$form.classList.add("form-open");
+    this.$noteTitle.style.display = "block";
+    this.$formButtons.style.display = "block";
+  }
 
-    closeForm() {
-      this.$form.classList.add('form-open');
-      this.$noteTitle.style.display = 'none';
-      this.$formButtons.style.display = 'none'; 
-    }
+  closeForm() {
+    this.$form.classList.remove("form-open");
+    this.$noteTitle.style.display = "none";
+    this.$formButtons.style.display = "none";
+  }
 
-    addNote(note) {
-      
-    }
+  addNote(note) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      color: "white",
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+    };
+    this.notes = [...this.notes, newNote];
+    this.displayNotes();
+  }
+  
+  displayNotes() {
+     const hasNotes = this.notes.length > 0;  
+     this.$placeholder.style.display = hasNotes ? 'none' : 'flex';  
+   
+     this.$notes.innerHTML = this.notes.map(note => `
+        <div style="background: ${note.color};" class="note">
+          <div class="${note.title && 'note-title'}">${note.title}</div>
+          <div class="note-text">${note.text}</div>
+          <div class="toolbar-container">
+            <div class="toolbar">
+              <img class="toolbar-color" src="https://icon.now.sh/palette">
+              <img class="toolbar-delete" src="https://icon.now.sh/delete">
+            </div>
+          </div>
+        </div>
+     `).join('');
+  }
 }
 
-new App()
+new App();
